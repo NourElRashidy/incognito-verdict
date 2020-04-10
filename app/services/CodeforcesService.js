5f0a83e955df8e150a3a54d235a255c7b440e8e8
Nour Elrashidy <elrashidyn@gmail.com>
2020-04-10 19:13:51 +0200

    Implement submitProblem method in CodeforcesService

--- app/services/CodeforcesService.js
+++ app/services/CodeforcesService.js
@@ -45,6 +45,47 @@ const openSessionForUser = async (user, pass) => {
     return isLoggedIn();
 }
 
+const closeRunningSession = async () => {
+    if (browser === null)
+        return;
+    await closeBrowser();
+}
+
+const LANGUAGE_DROPDOWN_SELECTOR = '#pageContent > form > table > tbody > tr:nth-child(3) > td:nth-child(2) > select';
+const SOURCE_CODE_SELECTOR = '#editor > div.ace_scroller > div > div.ace_layer.ace_text-layer > div';
+const SUBMIT_BUTTON = '#pageContent > form > table > tbody > tr:nth-child(6) > td > div > div > input';
+
+const submitProblem = async (contestId, problemId, languageId, sourceCode) => {
+    console.log('Submitting...');
+    try {
+        await page.goto(`https://codeforces.com/gym/${contestId}/submit/${problemId}`);
+        await page.select(LANGUAGE_DROPDOWN_SELECTOR, languageId);
+        await page.click(SOURCE_CODE_SELECTOR);
+        await page.keyboard.type(sourceCode);
+        await page.click(SUBMIT_BUTTON);
+    }
+    catch (e) {
+        throw new Error('Failed to submit!\n' + e);
+    }
+
+    if (page.url().includes('/my')) {
+        console.log('submitted successfully!');
+        return;
+    }
+
+    // Check if submission is rejected due to repeated submissions
+    try {
+        let element = await page.$('#pageContent > form > table > tbody > tr:nth-child(5) > td:nth-child(2) > div > span');
+        const err_text = await page.evaluate(element => element.textContent, element);
+        throw new Error(err_text);
+    }
+    catch (e) {
+        throw new Error('Failed to submit!\n' + e);
+    }
+}
+
 module.exports = {
-    openSessionForUser
+    openSessionForUser,
+    closeRunningSession,
+    submitProblem
 };
